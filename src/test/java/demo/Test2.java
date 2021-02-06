023b57745c584713aa89cd978b43dbee1c5bf05b
QSP <QSP@DESKTOP-EVK9GMC>
2021-02-06 12:54:19 +0530

    Sample

---
+++ src/test/java/demo/Test2.java
@@ -0,0 +1,11 @@
+package demo;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class Test2 {
+@Test
+public void demo() {
+	Reporter.log("hello python",true);
+}
+}
---
+++ src/test/java/demo/Test3.java
@@ -0,0 +1,11 @@
+package demo;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class Test3 {
+@Test
+public void demo() {
+	Reporter.log("hello Java",true);
+}
+}
